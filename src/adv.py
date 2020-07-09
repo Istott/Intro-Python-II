58831792ff40cbf560ad58e6d07be3b14ae1fe0b
isaac stott <isaac.cloyd@gmail.com>
2020-07-09 15:59:53 -0600

    added items to room

--- src/adv.py
+++ src/adv.py
@@ -1,25 +1,26 @@
 from room import Room
 from player import Player
+from item import Item
 
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-        "North of you, the cave mount beckons"),
+    'outside':  Room("Outside Cave Entrance ",
+        "North of you, the cave mount beckons", ['axe', 'sword']), 
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+    'foyer':    Room("Foyer. ", """Dim light filters in from the south. Dusty
+passages run north and east.""", ['penny', 'flashlight']),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+    'overlook': Room("Grand Overlook. ", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", ['umbrella', 'cane']),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+    'narrow':   Room("Narrow Passage. ", """The narrow passage bends here from west
+to north. The smell of gold permeates the air.""", ['vase', 'portrait']),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+    'treasure': Room("Treasure Chamber. ", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", ['feather', 'string']),
 }
 
 
@@ -40,16 +41,18 @@ room['treasure'].s_to = room['narrow']
 
 # Make a new player object that is currently in the 'outside' room.
 
+player = Player('bob', room['outside'])
+# item = Item(room.Room.Item)
+
 def set_direction(player, direction):
     attribute = direction + '_to'
 
     if hasattr(player.location, attribute):
         player.location = getattr(player.location, attribute)
     else:
-        print('oops.... the wall hurts when you hit it. try again')
+        print('\nYou`re not very smart are you?..... try again')
 
 
-player = Player('bob', room['outside'])
 # Write a loop that:
 #
 # * Prints the current room name
@@ -61,11 +64,13 @@ player = Player('bob', room['outside'])
 #
 # If the user enters "q", quit the game.
 # possible_direction('n', 's', 'w', 'e')
-direction = input('Welcome to the haunted Mansion....go north to enter: ')
+welcome = input('Welcome to the haunted Mansion....press enter to start ')
 
 while True:
     print('\n')
-    print(f'Hello {player.name}, you are in the {player.location} room.')
+    print(f'Hello {player.name}, you are in the {player.location}')
+    print('\nitems in this room: ')
+    player.location.itemList()
 
 
     command = input("\nWhat would you like to do: ").strip().lower().split()
@@ -73,6 +78,7 @@ while True:
     command = first_char[0]
 
     if command == 'q':
+        print('\nQuiters are for Losers!!!\n')
         break
 
     if command == 'n':
@@ -84,4 +90,4 @@ while True:
     elif command == 'w':
         set_direction(player, command)
     else:
-        print('oops wrong entry, try again')
+        print('\nYou make no sense, try again')
--- src/player.py
+++ src/player.py
@@ -4,12 +4,8 @@
 
 class Player():
     def __init__(self, name, location):
-        # super().__init__()
         self.name = name
         self.location = location
-    
-    # def try_direction(command):
-    #     attribute = command + '_to'
 
-    #     if hasattr(self.location, attribute):
-    #         getattr(self.location, attribute)
+    
+    
